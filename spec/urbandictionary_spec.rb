2b56221e2842d357bc215edf30eab84b69fec02f
Nick Charlton <hello@nickcharlton.net>
2013-08-28 23:58:49 +0100

    Adds a spec to test the current fetch method.

--- spec/urbandictionary_spec.rb
+++ spec/urbandictionary_spec.rb
@@ -10,4 +10,14 @@ describe 'UrbanDictionary' do
   after do
     VCR.eject_cassette
   end
+
+  it 'can fetch a definition' do
+    definition = @ud.get_top_definition('zomg')
+
+    # check there's a result
+    definition.wont_be_empty
+
+    # it should match the commonest first result
+    definition.must_match(/zOMG is a varient of the all-too-popular acronym/)
+  end
 end
