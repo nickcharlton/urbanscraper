9ac89cb3ce54b7f5005c4dddc640a8f84a718648
Nick Charlton <hello@nickcharlton.net>
2013-08-29 17:55:08 +0100

    Expands the parser to parse all of the definitions.
    * Keeps the tests passing as is.
    * But, now can return a full hash of the whole definition.

--- models/urbandictionary.rb
+++ models/urbandictionary.rb
@@ -1,23 +1,81 @@
 require 'nokogiri'
 require 'open-uri'
 require 'json'
+require 'date'
 
 class UrbanDictionary
   VERSION = '2.0pre'
   UA = "UrbanScraper/#{VERSION} (http://urbanscraper.herokuapp.com)"
-  URL = 'http://www.urbandictionary.com/define.php?term=' 
+  DOMAIN = 'http://www.urbandictionary.com'
+  URL = "#{DOMAIN}/define.php?term=" 
 
   def get_top_definition(term)
+    definitions = fetch_definitions(term)
+    top_definition = parse_definition(definitions[0])
+    top_definition[:definition]
+  end
+
+  private
+
+  def fetch_definitions(term)
     # pull it into nokogiri
     doc = Nokogiri::HTML(open(URL + term, 'User-Agent' => UA))
     
     # run the xpath
     entries_block = doc.xpath("/html/body//table[@id='entries']")
-    definition_group = entries_block.search("div[@class='definition']")
-    
-    definitions = Array.new
-    definition_group.each { |e| definitions << e.to_str }
+    definition_group = entries_block.search("td[@class='text']")
 
-    definitions[0]
+    # extract the actual definitions
+    definitions = Array.new
+    definition_group.each do |e|
+      # actual definitions have an id attribute like: "entry_123456".
+      id = e.attribute('id')
+      if id.value.start_with? "entry_"
+        definitions.push e
+      end
+    end
+
+    definitions
+  end
+
+  def parse_definition(definition)
+    # split the identifier
+    id = definition.attribute('id').value
+    id.sub!(/entry_/, '')
+    
+    # extract the definition and clean it up
+    definition_block = definition.search("div[@class='definition']")
+    definition_text = ""
+    definition_block.each { |e| definition_text << e.to_str }
+    definition_text.strip!
+
+    # extract the example
+    example_block = definition.search("div[@class='example']")
+    example = example_block.first.content
+    example.strip!
+
+    # extract the metadata block
+    metadata_block = definition.search("div[@class='greenery']")
+
+    # parse the author
+    author_block = metadata_block.search("a[@class='author']")
+    author = author_block.children.first.content
+    author_url = author_block.attribute('href').value
+    author_url = "#{DOMAIN}#{author_url}"
+
+    # post date
+    date_string_block = metadata_block.search("span[@class='date']")
+    date_string = date_string_block.first.content
+    date_string.strip!
+    date = DateTime.parse(date_string)
+
+    Hash[
+      :id => id, 
+      :definition => definition_text, 
+      :example => example, 
+      :author => author,
+      :author_url => author_url,
+      :posted => date
+    ]
   end
 end
