680925207935d907ab21b4ee178a5b5b3bc9ca96
Nick Charlton <hello@nickcharlton.net>
2013-08-29 09:42:33 +0100

    Replaces the Referer on the HTTP headers with a more complex UA.

--- models/urbandictionary.rb
+++ models/urbandictionary.rb
@@ -4,13 +4,12 @@ require 'json'
 
 class UrbanDictionary
   VERSION = '2.0pre'
-  UA = "UrbanScraper/#{VERSION} Ruby/#{RUBY_VERSION}"
-  REFERER = "http://urbanscraper.herokuapp.com/"
+  UA = "UrbanScraper/#{VERSION} (http://urbanscraper.herokuapp.com)"
   URL = 'http://www.urbandictionary.com/define.php?term=' 
 
   def get_top_definition(term)
     # pull it into nokogiri
-    doc = Nokogiri::HTML(open(URL + term, 'User-Agent' => UA, 'Referer' => REFERER))
+    doc = Nokogiri::HTML(open(URL + term, 'User-Agent' => UA))
     
     # run the xpath
     entries_block = doc.xpath("/html/body//table[@id='entries']")
